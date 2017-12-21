d9afb9af5f4c01567e0e2515aad4c24b1876c7ab
Brian McCann <bmccann36@gmail.com>
2017-12-21 15:58:42 -0500

    working on making twitter client cb a promise

--- fooler.js
+++ fooler.js
@@ -1,3 +1,4 @@
+const Twitter = require('twitter-node-client').Twitter;
 
 	//Callback functions
 	var error = function (err, response, body) {
@@ -10,14 +11,6 @@ var success = function (data) {
     console.log(result.length, 'length' )
     console.log(result[0].text)
 };
-
-var Twitter = require('twitter-node-client').Twitter;
-
-
-// make a directory in the root folder of your project called data
-// copy the node_modules/twitter-node-client/twitter_config file over into data/twitter_config`
-// Open `data/twitter_config` and supply your applications `consumerKey`, 'consumerSecret', 'accessToken', 'accessTokenSecret', 'callBackUrl' to the appropriate fields in your data/twitter_config file
-
 
 const keys = require('./data/twitter_config')
 
--- server/api/feeds.js
+++ server/api/feeds.js
@@ -1,8 +1,43 @@
 const router = require('express').Router()
 module.exports = router
+const Twitter = require('twitter-node-client').Twitter;
+const keys = require('../../data/twitter_config')
+var twitter = new Twitter(keys);
+
+let tweets;
+
+//Callback functions
+var error = function (err, response, body) {
+  console.log(err);
+};
+var success = function (data) {
+  return JSON.parse(data)
+};
+
+
+// twitter.getUserTimeline({ screen_name: 'JeffTweedy', count: '2' }, error, success);
+
+
 
 router.get('/', (req, res, next) => {
+  twitter.getUserTimeline({ screen_name: 'JeffTweedy', count: '2'}, error, success)
+  .then(()=> console.log('done'))
 
-     res.send('feeds')
-    .catch(next)
+  // .catch(next)
 })
+
+function getTweetsAsync() {
+  return new Promise(function (resolve, reject) {
+    twitter.getUserTimeline({ screen_name: 'JeffTweedy', count: '2' },error, success)
+
+}
+
+// function readFileAsync (file, encoding) {
+//   return new Promise(function (resolve, reject) {
+//     fs.readFile(file, encoding, function (err, data) {
+//       if (err) return reject(err) // rejects the promise with `err` as the reason
+//       resolve(data)               // fulfills the promise with `data` as the value
+//     })
+//   })
+// }
+// readFileAsync('myfile.txt').then(console.log, console.error)
